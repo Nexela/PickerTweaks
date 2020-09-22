1741ed0bca368190317e872c08684bad34f978c9
James <35714562+kirazy@users.noreply.github.com>
2020-09-21 18:51:23 -0700

    Hardcode filepaths to fix #10 (#11)

--- prototypes/entity-tweaks.lua
+++ prototypes/entity-tweaks.lua
@@ -1,10 +1,6 @@
 -- TODO Fixme nuclearReactorReactor.working_light_picture.filename nil
 local Data = require('__stdlib__/stdlib/data/data')
 
-local function get_new_path(oldTexturePath)
-    return (string.gsub(oldTexturePath, '^%_%_base%_%_', '__PickerTweaks__'))
-end
-
 if not mods['RealisticReactorGlow'] and settings.startup['picker-realistic-reactor-glow'].value then
     --[[
         "name": "RealisticReactorGlow",
@@ -13,15 +9,17 @@ if not mods['RealisticReactorGlow'] and settings.startup['picker-realistic-react
         "description": "The glow of working nuclear reactor has the proper color now!"
     --]]
     local nuclearReactorItem = Data("nuclear-reactor", "item")
-    nuclearReactorItem.icon = get_new_path(nuclearReactorItem.icon)
+    nuclearReactorItem.icon = "__PickerTweaks__/graphics/icons/nuclear-reactor.png"
     nuclearReactorItem.icon_size = 32
     nuclearReactorItem.icon_mipmaps = 1
 
     local nuclearReactorReactor = Data('nuclear-reactor', 'reactor')
     nuclearReactorReactor.light.color = {b = 0.94, g = 1, r = 0}
-    nuclearReactorReactor.working_light_picture.filename = get_new_path(nuclearReactorReactor.working_light_picture.filename)
-    nuclearReactorReactor.working_light_picture.hr_version.filename = get_new_path(nuclearReactorReactor.working_light_picture.hr_version.filename)
+    nuclearReactorReactor.working_light_picture.filename = "__PickerTweaks__/graphics/entity/nuclear-reactor/reactor-lights-color.png"
+    nuclearReactorReactor.working_light_picture.hr_version.filename = "__PickerTweaks__/graphics/entity/nuclear-reactor/hr-reactor-lights-color.png"
 
     local nuclearReactorTechnology = Data('nuclear-power', 'technology')
-    nuclearReactorTechnology.icon = get_new_path(nuclearReactorTechnology.icon)
+    nuclearReactorTechnology.icon = "__PickerTweaks__/graphics/technology/nuclear-power.png"
+    nuclearReactorTechnology.icon_size = 128
+    nuclearReactorTechnology.icon_mipmaps = 1
 end
